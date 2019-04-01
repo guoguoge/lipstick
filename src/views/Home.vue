76fdf45e7d425e35d144e45e4e3806d40b375160
zhangguo <506430706@qq.com>
2019-04-01 18:44:22 +0800

    修改

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,25 @@
 <template>
 <div class="">
-
+  <button-tab>
+    <button-tab-item>{{1 }}</button-tab-item>
+    <button-tab-item selected>{{ 2 }}</button-tab-item>
+    <button-tab-item>{{ 3 }}</button-tab-item>
+  </button-tab>
 </div>
 </template>
 
 <script>
-export default {}
+import {
+  ButtonTab,
+  ButtonTabItem
+} from 'vux'
+
+export default {
+  components: {
+    ButtonTab,
+    ButtonTabItem
+  }
+}
 </script>
 
 <style lang="less" scoped>
