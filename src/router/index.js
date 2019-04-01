a4d41fa19381da48528af1a0d51c023edaad6e38
zhangguo <506430706@qq.com>
2019-04-02 07:40:07 +0800

    页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import home from '../views/home.vue'
 
 
 const Layout = r => require.ensure([], () => r(require('../views/layout')), 'layout')
+const home = r => require.ensure([], () => r(require('../views/home')), 'home')
+const login = r => require.ensure([], () => r(require('../views/user/login')), 'login')
+const register = r => require.ensure([], () => r(require('../views/user/register')), 'register')
+const password = r => require.ensure([], () => r(require('../views/user/password')), 'password')
+
 
 Vue.use(Router)
 
@@ -22,14 +26,30 @@ export default new Router({
           title: '必势得云算力'
         }
       },
-      // {
-      //   path: '/market',
-      //   name: 'market',
-      //   component: market,
-      //   meta: {
-      //     title: '矿机商城-必势得'
-      //   },
-      // }
+      {
+        path: '/login',
+        name: 'login',
+        component: login,
+        meta: {
+          title: '登录'
+        },
+      }, ,
+      {
+        path: '/register',
+        name: 'register',
+        component: register,
+        meta: {
+          title: '注册'
+        },
+      },
+      {
+        path: '/password',
+        name: 'password',
+        component: password,
+        meta: {
+          title: '找回密码'
+        },
+      }
     ]
   }]
 })
---
+++ src/views/user/login.vue
@@ -0,0 +1,12 @@
+<template>
+<div class="">
+  登录
+</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less">
+</style>
